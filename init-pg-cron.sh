cc115cf83a3b0171b20a643d2b0796cccd721a14
Rushil Srivastava <rushu0922@gmail.com>
2024-12-08 16:38:16 -0800

    :white_check_mark: Add tests

--- init-pg-cron.sh
+++ init-pg-cron.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 set -e
 
-# First, add pg_cron to shared_preload_libraries
-echo "shared_preload_libraries = 'pg_cron'" >> ${PGDATA}/postgresql.conf
+# Enable pg_cron extension in shared_preload_libraries
+sed -i "s/#shared_preload_libraries = ''/shared_preload_libraries = 'pg_cron'/g" ${PGDATA}/postgresql.conf
 
 # Restart PostgreSQL to load the library
 pg_ctl -D ${PGDATA} -m fast -w restart
 
-# Now create the extension and configure it
+# Now create the extension
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    -- First create the extension
+    -- Create the extension
     CREATE EXTENSION IF NOT EXISTS pg_cron;
-    
-    -- Now we can safely configure it
-    ALTER SYSTEM SET cron.database_name = 'postgres';
-    SELECT pg_reload_conf();
+EOSQL
+
+# Configure pg_cron after extension is created
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    -- Set the database where the pg_cron background worker will run
+    SELECT cron.set_database('postgres');
 EOSQL
 
 # Final restart to ensure all settings are applied
